2d78762cff50ae161e498a87d0ca00826a2faf7c
Nabil Md Shahid <53196922+nabil404@users.noreply.github.com>
2021-03-28 13:36:28 +0600

    push pop added to doubly linked list

--- src/DoublyLinkedList/index.ts
+++ src/DoublyLinkedList/index.ts
@@ -13,4 +13,37 @@ export class DoublyLinkedList<T> {
     this._tail = null;
     this._length = 0;
   }
+
+  get length(): number {
+    return this._length;
+  }
+
+  push(value: T): Link<T> {
+    const newNode = new Link<T>(value);
+    if (!this._head) {
+      this._head = newNode;
+      this._tail = this._head;
+    } else {
+      this._tail!.next = newNode;
+      newNode.prev = this._tail;
+      this._tail = newNode;
+    }
+    this._length++;
+    return newNode;
+  }
+
+  pop(): Link<T> {
+    this._isEmpty();
+    const poppedNode = this._tail;
+    if (this._length === 1) {
+      this._head = null;
+      this._tail = null;
+    } else {
+      this._tail = poppedNode!.prev;
+      this._tail!.next = null;
+      poppedNode!.prev = null;
+    }
+    this._length--;
+    return poppedNode!;
+  }
 }
